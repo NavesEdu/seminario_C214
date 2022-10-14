02aab5c4fc643c8df7aa357b8c86061607d1301a
Matheus Chagas <matheusilva431+github@gmail.com>
2022-10-14 01:39:23 -0300

    feat: add Class Calculadora

---
+++ src/main/kotlin/br/inatel/seminario/c214/seminario_C214/Calculadora.kt
@@ -0,0 +1,14 @@
+class Calculator {
+    var num1: Int,
+    var num2: Int
+
+    fun sum(num1, num2) = num1 + num2
+
+    fun sub(num1, num2) = num1 - num2
+
+    fun mult(num1, num2) = num1 * num2
+
+    fun div(num1, num2) = num1 / num2
+
+}
+
