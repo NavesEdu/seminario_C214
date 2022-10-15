d73f72b0a788816c20b72b452cdf537c09b6306b
GabrielHS <gabrielhenri0012@gmail.com>
2022-10-15 18:06:46 -0300

    draft: controller

--- src/main/kotlin/br/inatel/seminario/c214/seminario_C214/controllers/ItemController.kt
+++ src/main/kotlin/br/inatel/seminario/c214/seminario_C214/controllers/ItemController.kt
@@ -1,16 +1,18 @@
 package br.inatel.seminario.c214.seminario_C214.controllers
 
 import br.inatel.seminario.c214.seminario_C214.entities.Item
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.http.HttpResponse
 
 @RestController
 @RequestMapping("/item")
 class ItemController {
 
     @PostMapping
-    fun addNewItem(item: Item){
-
-    }
+    fun addNewItem(item: Item): ResponseEntity<Item> = ResponseEntity.ok().body(item);
 }
--- src/main/kotlin/br/inatel/seminario/c214/seminario_C214/entities/Item.kt
+++ src/main/kotlin/br/inatel/seminario/c214/seminario_C214/entities/Item.kt
@@ -1,16 +1,19 @@
 package br.inatel.seminario.c214.seminario_C214.entities
 
+import io.swagger.v3.oas.annotations.media.Schema
+import java.io.Serializable
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class Item (
+data class Item(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null,
-    val name: String,
-    val desc: String,
-    val price: Float
+    @Schema(hidden = true)
+    private val id: Long? = null,
+    private val name: String,
+    private val desc: String,
+    private val price: Float
 )
