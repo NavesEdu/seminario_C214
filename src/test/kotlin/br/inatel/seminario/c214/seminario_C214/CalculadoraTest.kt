6cebb7948d7d09b38ca77517f1d256b2d83115ed
GabrielHS <gabrielhenri0012@gmail.com>
2022-10-15 18:21:39 -0300

    fix: test

--- src/test/kotlin/br/inatel/seminario/c214/seminario_C214/CalculadoraTest.kt
+++ src/test/kotlin/br/inatel/seminario/c214/seminario_C214/CalculadoraTest.kt
@@ -1,36 +1,38 @@
 package br.inatel.seminario.c214.seminario_C214.Calculadora
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import Calculadora
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+
+
 
 class CalculadoraTest{
 
     @Test
-    fun 'should sum 4 plus 9'(){
+    fun `should sum 4 plus 9`(){
         val calc = Calculadora()
-        val result = calc.sum(2, 2)
-        assertEquals(13, result)
+        val result = calc.sum(4, 9)
+        Assertions.assertEquals(13, result)
     }
 
     @Test
-    fun 'should subtract 5 by 3'(){
+    fun `should subtract 5 by 3`(){
         val calc = Calculadora()
-        val result = calc.sub(2, 2)
-        assertEquals(2, result)
+        val result = calc.sub(5, 3)
+        Assertions.assertEquals(2, result)
     }
 
     @Test
-    fun 'should multiply 2 by 2'(){
+    fun `should multiply 2 by 2`(){
         val calc = Calculadora()
         val result = calc.mult(2, 2)
-        assertEquals(4, result)
+        Assertions.assertEquals(4, result)
     }
 
     @Test
-    fun 'should divide 10 by 5'(){
+    fun `should divide 10 by 5`(){
         val calc = Calculadora()
         val result = calc.div(10, 5)
-        assertEquals(2, result)
+        Assertions.assertEquals(2, result)
     }
 }
