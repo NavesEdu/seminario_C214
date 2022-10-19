223ef3ed463742e0f331fdb99135caed5c7bd44a
Matheus Chagas <matheusilva431+github@gmail.com>
2022-10-19 03:33:14 -0300

    test: Adicionando o throw na funcao div

--- src/main/kotlin/br/inatel/seminario/c214/seminario_C214/Calculadora.kt
+++ src/main/kotlin/br/inatel/seminario/c214/seminario_C214/Calculadora.kt
@@ -1,12 +1,19 @@
 class Calculadora {
 
-    fun sum(num1: Int, num2: Int) = num1 + num2
+    fun sum(num1: Double, num2: Double) = num1 + num2
 
-    fun sub(num1: Int, num2: Int) = num1 - num2
+    fun sub(num1: Double, num2: Double) = num1 - num2
 
-    fun mult(num1: Int, num2: Int) = num1 * num2
+    fun mult(num1: Double, num2: Double) = num1 * num2
 
-    fun div(num1: Int, num2: Int) = num1 / num2
+    fun div(num1: Double, num2: Double): Any {
+        return if(num2 != 0.0){
+            num1 / num2
+        }
+        else{
+            throw ArithmeticException("Não é possível dividir por zero")
+        }
+    }
 
 }
 
--- src/test/kotlin/br/inatel/seminario/c214/seminario_C214/CalculadoraTest.kt
+++ src/test/kotlin/br/inatel/seminario/c214/seminario_C214/CalculadoraTest.kt
@@ -1,38 +1,50 @@
 package br.inatel.seminario.c214.seminario_C214.Calculadora
 
 import Calculadora
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
+import org.junit.jupiter.api.assertThrows
 
 
 class CalculadoraTest{
 
     @Test
-    fun `should sum 4 plus 9`(){
+    fun `some 4 com 9`(){
         val calc = Calculadora()
-        val result = calc.sum(4, 9)
-        Assertions.assertEquals(13, result)
+        val result = calc.sum(4.0, 9.0)
+        assertEquals(13.0, result)
     }
 
     @Test
-    fun `should subtract 5 by 3`(){
+    fun some_25_com_30(){
         val calc = Calculadora()
-        val result = calc.sub(5, 3)
-        Assertions.assertEquals(2, result)
+        val result = calc.sum(25.0, 30.0)
+        assertEquals(50.0, result)
     }
 
     @Test
-    fun `should multiply 2 by 2`(){
+    fun `subtraia 3 de 5`(){
         val calc = Calculadora()
-        val result = calc.mult(2, 2)
-        Assertions.assertEquals(4, result)
+        val result = calc.sub(5.0, 3.0)
+        assertEquals(2.0, result)
     }
 
     @Test
-    fun `should divide 10 by 5`(){
+    fun `multiplique 2 por 2`(){
         val calc = Calculadora()
-        val result = calc.div(10, 5)
-        Assertions.assertEquals(2, result)
+        val result = calc.mult(2.0, 2.0)
+        assertEquals(4.0, result)
+    }
+
+    @Test
+    fun `Divida 15 por 4, com a função div throw`(){
+        val calc = Calculadora()
+        val result = calc.div(15.0, 4.0)
+        assertEquals(3.75, result)
+    }
+    @Test
+    fun `Divida 15 por 0, com o retorno do throw`(){
+        val calc = Calculadora()
+        assertThrows<ArithmeticException> { calc.div(15.0, 0.0) }
     }
 }
